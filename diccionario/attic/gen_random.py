f9a46d0703126f571343b2b188c879284e348a6c
Nico Lehmann <nico.lehmannm@gmail.com>
2018-10-29 20:38:26 -0700

    Arreglos testplan de diccionario

--- diccionario/attic/gen_random.py
+++ diccionario/attic/gen_random.py
@@ -3,10 +3,10 @@ import random
 import string
 from collections import OrderedDict
 
-def potoconcaca(size=1, chars=string.ascii_lowercase):
+def gen_word(size=1, chars=string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
-random.seed(hash(str(sys.argv[0]))) #fijar la semillita para no cagarse a nadie por aleatoriedad.
+random.seed(sys.argv[1]) #fijar la semilla
 
 M = int(sys.argv[2]); # tamano del diccionario max (se puede achicar por repeticiones)
 N = int(sys.argv[3]); # tamano del texto approx
@@ -26,15 +26,13 @@ dic0=[];
 lenguaje = string.ascii_lowercase;
 if(restric!=0):
 	lenguaje = lang;
-print(lenguaje);
 if(b!=0):
 	for a in range(0,M):
-		dic0.append(potoconcaca(L,lenguaje));
-		
+		dic0.append(gen_word(L,lenguaje));
+
 if(b==0):
 	for a in range(0,M):
-		dic0.append(potoconcaca(random.choice(range(1,L+1)),lenguaje));
-#print(dic0);
+		dic0.append(gen_word(random.choice(range(1,L+1)),lenguaje));
 dic = list(OrderedDict.fromkeys(dic0))
 dic.sort();
 M = len(dic);
@@ -47,7 +45,7 @@ while(l+len(t) <=N):
 	t = random.choice(dic);
 N = len(S);
 if(basura!=0):
-	S = potoconcaca(N);
+	S = gen_word(N);
 print(M);
 for d in dic:
 	print(d);
