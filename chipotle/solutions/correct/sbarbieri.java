7531e9b15361cb5faa89da71874d82559e4cccf7
vardieri <sbarbiericl@gmail.com>
2018-11-04 14:01:02 -0800

    modulo en sol

--- chipotle/solutions/correct/sbarbieri.java
+++ chipotle/solutions/correct/sbarbieri.java
@@ -10,22 +10,24 @@ class chipotle {
 	public static String s;
 	public static int M,l;
 	public static String[] dic;
-	public static int[] c;
+	public static long[] c;
+	public static long mod;
 
 
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(System.in);
+		mod = 1000009;
 		M = in.nextInt();
 		dic = new String [M];
 		for(int i =0; i<M; i++){dic[i]=in.next();}
 		s = in.next();
-		c = new int[s.length()+1];
+		c = new long[s.length()+1];
 		c[s.length()]=1;
 		for(int i =s.length()-1; i >=0; i--){
 			for(int j =0; j<M; j++){
 				l = dic[j].length();
 				if(i+l <= s.length()){
-					if(s.substring(i,i+l).equals(dic[j])){c[i]+=c[i+l];}
+					if(s.substring(i,i+l).equals(dic[j])){c[i]=(c[i]+c[i+l])%mod;}
 				}
 			}
 			//System.out.println(i+" "+c[i]);
